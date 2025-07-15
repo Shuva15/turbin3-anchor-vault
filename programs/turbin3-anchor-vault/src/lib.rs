dc863246e395f3a157891c3e964ad239f70f7fd9
shuva <mahantashuva@gmail.com>
2025-07-15 14:50:57 +0530

    Wrote all the tests and fixed bugs in the contract

--- programs/turbin3-anchor-vault/src/lib.rs
+++ programs/turbin3-anchor-vault/src/lib.rs
@@ -50,16 +50,16 @@ pub struct Payment<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
         mut,
         seeds = [b"vault", vault_state.key().as_ref()],
         bump = vault_state.vault_bump
     )]
     pub vault: SystemAccount<'info>,
-    #[account(
-        seeds = [b"state", user.key().as_ref()],
-        bump = vault_state.vault_bump
-    )]
-    pub vault_state: Account<'info, VaultState>,
     pub system_program: Program<'info, System>
 }
 
@@ -69,17 +69,17 @@ pub struct Close<'info> {
     pub user: Signer<'info>,
     #[account(
         mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
         seeds = [b"vault", vault_state.key().as_ref()],
         bump = vault_state.vault_bump
     )]
     pub vault: SystemAccount<'info>,
-    #[account(
-        mut,
-        seeds = [b"state", user.key().as_ref()],
-        bump = vault_state.vault_bump,
-        close = user
-    )]
-    pub vault_state: Account<'info, VaultState>,
     pub system_program: Program<'info, System>
 }
 
@@ -131,7 +131,7 @@ impl<'info> Payment<'info> {
         from: self.vault.to_account_info(),
         to: self.user.to_account_info()
         };
-        let seeds = &[b"vault", self.user.to_account_info().key.as_ref(), &[self.vault_state.vault_bump],];
+        let seeds = &[b"vault", self.vault_state.to_account_info().key.as_ref(), &[self.vault_state.vault_bump],];
         let signer_seeds = &[&seeds[..]];
 
         let cpi_ctx = CpiContext::new_with_signer(self.system_program.to_account_info(), cpi_accounts, signer_seeds);
@@ -146,7 +146,7 @@ impl<'info> Close<'info> {
             from: self.vault.to_account_info(),
             to: self.user.to_account_info()
         };
-        let seeds = &[b"vault", self.user.to_account_info().key.as_ref(), &[self.vault_state.vault_bump],];
+        let seeds = &[b"vault", self.vault_state.to_account_info().key.as_ref(), &[self.vault_state.vault_bump],];
         let signer_seeds = &[&seeds[..]];
 
         let cpi_ctx = CpiContext::new_with_signer(self.system_program.to_account_info(), cpi_accounts, signer_seeds);
--- tests/turbin3-anchor-vault.ts
+++ tests/turbin3-anchor-vault.ts
@@ -1,16 +1,111 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Turbin3AnchorVault } from "../target/types/turbin3_anchor_vault";
+import { assert } from "chai";
 
 describe("turbin3-anchor-vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.turbin3AnchorVault as Program<Turbin3AnchorVault>;
+  const program = anchor.workspace
+    .turbin3AnchorVault as Program<Turbin3AnchorVault>;
+
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("state"), provider.publicKey.toBytes()],
+    program.programId
+  )[0];
+
+  const vault = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), vaultState.toBytes()],
+    program.programId
+  )[0];
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accountsPartial({
+      user: provider.publicKey,
+      vaultState,
+      vault,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+    console.log("Initialization transaction signature", tx);
   });
+
+  it("Deposit Lamports", async () => {
+    const amount = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL)
+    const tx = await program.methods.deposit(amount).accountsPartial({
+      user: provider.publicKey,
+      vaultState,
+      vault,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+    console.log("Deposit transaction signature", tx);
+    const vaultBalance = await provider.connection.getBalance(vault);
+    console.log(`Vault balance after deposit: ${ vaultBalance }, lamports`);
+  });
+
+  it("Try to withdraw more then vault balance", async () => {
+    try {
+      const amount = new anchor.BN(2 * anchor.web3.LAMPORTS_PER_SOL)
+
+      const tx = await program.methods.withdraw(amount).accountsPartial({
+        user: provider.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }).rpc();
+
+      assert.fail("Expected transaction to fail but it succeeded");
+    } catch (err) {
+      const anchorErr = err as anchor.AnchorError;
+
+      assert.equal(anchorErr.error.errorCode.code, "InsufficientVaultBalance")
+    }
+  });
+
+  it("Try to withdraw including rent", async () => {
+    try {
+      const amount = new anchor.BN(100 + anchor.web3.LAMPORTS_PER_SOL)
+
+      const tx = await program.methods.withdraw(amount).accountsPartial({
+        user: provider.publicKey,
+        vaultState,
+        vault,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }).rpc();
+
+      assert.fail("Expected transaction to fail but it succeeded");
+    } catch (err) {
+      const anchorErr = err as anchor.AnchorError;
+
+      assert.equal(anchorErr.error.errorCode.code, "BelowRentExemption")
+    }
+  });
+
+  it("Withdraw from vault", async () => {
+    const amount = new anchor.BN(anchor.web3.LAMPORTS_PER_SOL)
+
+    const tx = await program.methods.withdraw(amount).accountsPartial({
+      user: provider.publicKey,
+      vaultState,
+      vault,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
+    console.log("Withdraw transaction signature", tx);
+    const vaultBalance = await provider.connection.getBalance(vault);
+    console.log(`Vault balance after withdraw: ${ vaultBalance }, lamports`);
+  });
+
+  it("Close PDA accounts", async () => {
+    const tx = await program.methods.close().accountsPartial({
+      user: provider.publicKey,
+      vaultState,
+      vault,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc()
+
+    console.log("Close Accounts transaction signature", tx);
+    console.log("Vault account info after closing:", await provider.connection.getAccountInfo(vault));
+    console.log("Vault state should be null (closed):", await provider.connection.getAccountInfo(vaultState));
+  })
 });
